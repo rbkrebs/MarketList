a12ac31c536dde520baf7c117d4b0c4b5ba0bc0c
Romulo Krebs <romulo.krebs@gmail.com>
2020-11-02 11:14:26 -0300

    insere ações de click em lista sugerida

--- src/main/java/com/example/marketlist/activities/ListaSugerida.java
+++ src/main/java/com/example/marketlist/activities/ListaSugerida.java
@@ -7,11 +7,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 
 import com.example.marketlist.adapters.BasicAdapter;
 import com.example.marketlist.R;
+import com.example.marketlist.listener.RecyclerItemClickListener;
 import com.example.marketlist.model.ListaProvisoria;
+import com.google.android.material.snackbar.Snackbar;
 
 public class ListaSugerida extends AppCompatActivity {
 
@@ -35,6 +38,30 @@ public class ListaSugerida extends AppCompatActivity {
         listaMercado.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         listaMercado.setLayoutManager(layoutManager);
+
+        listaMercado.addOnItemTouchListener(new RecyclerItemClickListener(getApplicationContext(),
+                listaMercado, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Snackbar snackbar = Snackbar.make(view, "Pressione no item para adicioná=lo",
+                        Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+                Snackbar snackbar = Snackbar.make(view, "Item adicionado... mentirinha!!!!",
+                        Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        }));
+
+
 
 
         adicionaItem = findViewById(R.id.cria_item);
