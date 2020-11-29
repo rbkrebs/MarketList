939598728a979e151b17d991e86379eb5f2f0562
Romulo Krebs <romulo.krebs@mail.com>
2020-11-29 11:02:47 -0300

    implementa parent activity

---
+++ src/main/java/com/example/marketlist/activities/Login.java
@@ -0,0 +1,16 @@
+package com.example.marketlist.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.marketlist.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
